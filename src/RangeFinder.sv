5b9dda34b514bc1e9d76ceb8ccfae7659fa714ef
Alden G <aldengrover@gmail.com>
2026-02-22 11:59:07 -0500

    added RangeFinder

--- src/RangeFinder.sv
+++ src/RangeFinder.sv
@@ -1,10 +1,3 @@
-/*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
-
-`default_nettype none
-
 module RangeFinder
    #(parameter WIDTH=16)
     (input  logic [WIDTH-1:0] data_in,
@@ -99,43 +92,3 @@ module RangeFinder
     // difference
     assign range = max - min;
 endmodule: RangeFinder
-
-module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
-);
-
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uio_oe[0] = 1'b0;
-  assign uio_oe[1] = 1'b0;
-  assign uio_oe[2] = 1'b1;
-  assign uio_oe[3] = 1'b0; // unused
-  assign uio_oe[4] = 1'b0; // usused
-  assign uio_oe[5] = 1'b0; // unused
-  assign uio_oe[6] = 1'b0; // unused
-  assign uio_oe[7] = 1'b0; // unused
-
-  assign uio_out[0] = 1'b0;
-  assign uio_out[1] = 1'b0;
-  assign uio_out[3] = 1'b0;
-  assign uio_out[4] = 1'b0;
-  assign uio_out[5] = 1'b0;
-  assign uio_out[6] = 1'b0;
-  assign uio_out[7] = 1'b0;
-
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
-
-  wire rst;
-  wire [7:0] high_q, low_q;
-  assign rst = ~rst_n;
-
-  RangeFinder #(8) rf(.data_in(ui_in), .clock(clk), .reset(rst), .go(uio_in[0]), .finish(uio_in[1]), .error(uio_out[2]), .range(uo_out), .high_q(high_q), .low_q(low_q));
-
-endmodule
